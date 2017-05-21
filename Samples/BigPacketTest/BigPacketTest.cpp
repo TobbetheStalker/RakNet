976d8d679b86f3f9d869000d47a8f34915f5d472
TobbetheStalker <tobbe.kling@hotmail.com>
2017-05-21 13:27:35 +0200

    UPDATE send one GB packet

--- Samples/BigPacketTest/BigPacketTest.cpp
+++ Samples/BigPacketTest/BigPacketTest.cpp
@@ -24,7 +24,7 @@
 bool quit;
 bool sentPacket=false;
 
-#define BIG_PACKET_SIZE 83296256 //1073741824
+#define BIG_PACKET_SIZE 1073741824 //1073741824
 
 using namespace RakNet;
 
@@ -156,11 +156,11 @@ int main(void)
 						text[0]=(unsigned char) 255;
 
 
-					int nrofpackets = 1073741824 / BIG_PACKET_SIZE + 1;
-					for (int i = 0; i < nrofpackets; i++)
-					{
+					//int nrofpackets = 1073741824 / BIG_PACKET_SIZE + 1;
+					//for (int i = 0; i < nrofpackets; i++)
+					//{
 						server->Send(text, BIG_PACKET_SIZE, LOW_PRIORITY, RELIABLE_ORDERED_WITH_ACK_RECEIPT, 0, packet->systemAddress, false);
-					}
+					//}
 					
 					// Keep the stat from updating until the messages move to the thread or it quits right away
 					nextStatTime=RakNet::GetTimeMS()+1000;
